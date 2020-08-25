1996616c82d6dff8573f6bed55e8e73f7591243d
Ebert <ebertcrn@gmail.com>
2020-08-25 19:25:15 -0300

    Password Generator

---
+++ password_generator-project/generator/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+def home(request):
+    # return HttpResponse("Hello!")
+    return render(request, 'generator/home.html', {'password': 'udi2192'})   # render vai direto para PROJECT_NAME/APP_NAME/templates
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('letraMaiuscula'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('numeros'):
+        characters.extend(list('0123456789'))
+    if request.GET.get('caracteresEspeciais'):
+        characters.extend(list('!@#$%&*'))
+
+    length = int(request.GET.get('comprimento', 10))  # default value = 10
+    thepassword = ''
+
+    for eachChar in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
+
+def about(request):
+    return render(request, 'generator/about.html')
