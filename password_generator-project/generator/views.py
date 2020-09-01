43f0871c6a4d0d81e5e44654401686003dbde754
Ebert <ebertcrn@gmail.com>
2020-09-01 16:54:29 -0300

    Translation to english

--- password_generator-project/generator/views.py
+++ password_generator-project/generator/views.py
@@ -11,16 +11,16 @@ def home(request):
 def password(request):
     characters = []
 
-    if request.GET.get('letraMinuscula'):
+    if request.GET.get('lowercase'):
         characters.extend(list('abcdefghijklmnopqrstuvwxyz'))
-    if request.GET.get('letraMaiuscula'):
+    if request.GET.get('uppercase'):
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-    if request.GET.get('numeros'):
+    if request.GET.get('numbers'):
         characters.extend(list('0123456789'))
-    if request.GET.get('caracteresEspeciais'):
+    if request.GET.get('specialchars'):
         characters.extend(list('!@#$%&*'))
 
-    length = int(request.GET.get('comprimento', 10))  # default value = 10
+    length = int(request.GET.get('length', 10))  # default value = 10
     thepassword = ''
 
     if not len(characters):
